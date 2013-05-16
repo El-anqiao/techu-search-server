cb118f646c698e127ca808f7d4320c04d33b57f4
georgepsarakis <giwrgos.psarakis@gmail.com>
2013-05-16 23:42:05 +0300

    Github project overview page updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  $('pre.html').snippet('html');
+  $('.example-toggle').click(function(){
+    el = $(this).parents('.example-box').find('.example-result');
+    $(el).slideToggle();
+    if ( $(el).css('display') == 'none' ){
+      $(this).removeClass('minus').addClass('plus');
+    } else {
+      $(this).addClass('minus').removeClass('plus');
+    }
+  });
 });
