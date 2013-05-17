622f49128a0831f7deba7fe7f0f48fb0c737d7aa
georgepsarakis <giwrgos.psarakis@gmail.com>
2013-05-17 23:14:55 +0300

    Github project overview page updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
   $('.example-toggle').click(function(){
     el = $(this).parents('.example-box').find('.example-result');
-    $(el).slideToggle();
-    if ( $(el).css('display') == 'none' ){
+    if ( $(el).css('display') != 'none' ){
       $(this).removeClass('minus').addClass('plus');
     } else {
       $(this).addClass('minus').removeClass('plus');
     }
+    $(el).slideToggle();
   });
+  $('#how-it-works-diagram-container').height($('#how-it-works-text').height());
 });
